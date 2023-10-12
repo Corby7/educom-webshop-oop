2f836ae489f06ab9f31573b356f8cb9ebd07811d
Corby7 <corbijn.bulsink@hotmail.com>
2023-10-12 11:34:09 +0200

    added makeOrder functions #8

--- models/shopmodel.php
+++ models/shopmodel.php
@@ -9,7 +9,7 @@ class ShopModel extends PageModel {
     public $products = array();
     public $product = array();
     public $cartLines = array();
-    public $cartTotal = array();
+    public $cartTotal = 0;
 
     public function isUserLoggedIn() {
         return $this->sessionManager->isUserLoggedIn();
@@ -35,56 +35,70 @@ class ShopModel extends PageModel {
     }
 
     public function handleCartActions() {
-        $action = $this->getPostVar("id");
+        $action = $this->getPostVar("action");
 
         switch($action) {
             
             case 'addtocart':
-                $productid = getPostVar("id");
+                $productid = $this->getPostVar("id");
                 $this->sessionManager->addToCart($productid);
                 break;
 
             case 'removefromcart':
-                $productid = getPostVar("id");
+                $productid = $this->getPostVar("id");
                 $this->sessionManager->removeFromCart($productid);
                 break;
             
-            //deze nog aanpassen
-            // case 'checkout':
-            //     require_once('userservice.php');
-            //     if (makeOrder()) {
-            //         $_SESSION['shoppingcart'] = array();
-            //         return true;
-            //     }
-            //     break;
+            case 'checkout':
+                if ($this->makeOrder()) {
+                    $_SESSION['shoppingcart'] = array();
+                    return true;
+                }
+                break;
+        }
+    }
+
+    
+    public function makeOrder() {
+        try {
+            $cart = $_SESSION['shoppingcart'];
+            $email = $_SESSION['useremail'];
+            require_once("mysqlconnect.php");
+            //maybe just use private user id i saved somewhere?
+            $useridArray = getUserId($email);
+            $useridString = reset($useridArray);
+            createOrder($useridString, $cart);
+            return true;
+        } catch (Exception $e) {
+            logError("Checkout failed: " . $e->getMessage());
+            return false;
         }
     }
 
 
-    public function populateCart() {
+    public function getCartLines() {
         try {
-            $cartLines = array();
-            $total = 0; 
-    
             $cart = $this->sessionManager->getCart();
+            
             if (empty($cart)) {
-                return compact('cartLines', 'total');  
+                return;
             }
             $productids = array_keys($cart);
+            require_once("mysqlconnect.php");
             $cartProducts = getProductsByIds($productids);
             foreach($cart as $productid => $quantity) {
-               $product = $cartProducts[$productid];  
-               extract($product);                       
-               $subtotal = $price * $quantity; 
-               $total += $subtotal;
-               $cartLines[] = compact('id', 'filenameimage', 'name', 'price', 'quantity', 'subtotal');
+                $product = $cartProducts[$productid];  
+                extract($product);                       
+                $subtotal = $price * $quantity; 
+                $this->cartTotal += $subtotal;
+                $this->cartLines[] = compact('id', 'filenameimage', 'name', 'price', 'quantity', 'subtotal');
             } 
-            return compact('cartLines', 'total');  
         } catch (Exception $e) {
             logError("Getting cart products failed: " . $e->getMessage()); 
             return array('genericErr' => 'door een technische storing kunnen wij uw winkelwagen niet laten zien');
         }
     }
+
 }
 
 ?>
