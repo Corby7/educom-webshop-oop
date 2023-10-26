f5eb622c421cb2e6397b3e552e0181639b3074ba
Corby7 <corbijn.bulsink@hotmail.com>
2023-10-26 15:56:06 +0200

    added get and fillstars function #12

--- demo.js
+++ demo.js
@@ -1,17 +1,48 @@
 $(document).ready(function() {
+
+  productID = $(".rating").attr('data-productid');
+  console.log(productID);
+
+
+    $.ajax({
+      type: 'GET',
+      url: "index.php?action=ajax&function=getRating&id=4>",
+      success: function($ratingValue) {
+        console.log('succes', $ratingValue);
+        fillStars(parseInt($ratingValue));
+      }
+    });
+
+    function fillStars($value) {
+      $('.star').each((index, elem) => {
+        const itemValue = $(elem).attr('data-value')
+        if(itemValue <= $value) {
+          $(elem).addClass('red')
+        }
+      });
+    }
+
    
-    $(".star").click( function() {
+    $(".star").click(function() {
         const value = $(this).attr('data-value')
 
+        //clear all stars on click
         $(".star").removeClass('red')
         
-        $('.star').each( (index, elem) => {
-            const itemValue = $(elem).attr('data-value')
-            if(itemValue <= value) {
-              $(elem).addClass('red')
-            }
-          })
+        //paint all stars red
+        fillStars(value);
+
+        $.ajax({
+          type: "POST",
+          url: "index.php?action=ajax",
+          data: { rating: value },
+          success: function(result) {
+            //code dat runned op succes
+          }
+        });
     })
 
  });
+
+
 
