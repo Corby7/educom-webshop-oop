73533697d3d48e661a15e2609d96927916069876
Corby7 <corbijn.bulsink@hotmail.com>
2023-10-26 15:55:23 +0200

    created ratingcrud skeleton #12

---
+++ cruds/ratingcrud.php
@@ -0,0 +1,15 @@
+<?php
+
+class RatingCrud {
+
+public function saveRatingProduct($productId, $UserId, $rating) {}
+
+public function adjustRatingProduct($productId, $userId, $rating) {}
+
+public function getProductRating($productId) {}
+
+public function getAllRatings($productIds) {}
+
+}
+
+?>
