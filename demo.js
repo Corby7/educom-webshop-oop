45028a5fa0525253d0cd3c447f1c29db697b4d86
Corby7 <corbijn.bulsink@hotmail.com>
2023-10-27 17:35:34 +0200

    finished updaterating function #12

--- demo.js
+++ demo.js
@@ -44,9 +44,15 @@ $(document).ready(function() {
     });
   }
 
+  updateRating();
+
   function updateRating() {
+    const productId = $('.rating').attr('data-productid');
+    console.log(productId);
+
     $(".star").click(function() {
         const value = $(this).attr('data-value')
+        console.log(value);
 
         //clear all stars on click
         $(".star").removeClass('red')
@@ -56,8 +62,8 @@ $(document).ready(function() {
 
         $.ajax({
           type: "POST",
-          url: "index.php?action=ajax",
-          data: { rating: value },
+          url: "index.php?action=ajax&function=updateRating&id=" + productId + "&rating=" + value,
+          //data: { rating: value },
           success: function(result) {
             //code dat runned op succes
           }
