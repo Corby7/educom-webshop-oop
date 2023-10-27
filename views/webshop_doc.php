35b12691ffe69f57f56fb42856f701e6722595db
Corby7 <corbijn.bulsink@hotmail.com>
2023-10-27 18:02:33 +0200

    changed styling webshop #12

--- views/webshop_doc.php
+++ views/webshop_doc.php
@@ -19,19 +19,19 @@ class WebshopDoc extends ProductDoc {
                     <a href="index.php?page=productpage&productid=' . $product->id . '" class="productlink text-decoration-none">
                         <img src="images/' . $product->filenameimage . '" class="img-fluid" style="width: 400px" alt="Image of ' . $product->name . '">
                     </a>
-                    <div class="card-body d-flex flex-wrap g-4 justify-content-between align-items-center">
+                    <div class="card-body d-flex justify-content-between m-1">
                         <div class="d-flex flex-column">
                             <span class="card-title h2">' . $product->name . '</span>
                             <div class="rating" data-productid="' . $product->id . '">
-                                <span class="star" data-value="1">*</span>
-                                <span class="star" data-value="2">*</span>
-                                <span class="star" data-value="3">*</span>
-                                <span class="star" data-value="4">*</span>
-                                <span class="star" data-value="5">*</span>
+                                <span class="star" data-value="1"><i class="bi bi-star"></i></span>
+                                <span class="star" data-value="2"><i class="bi bi-star"></i></span>
+                                <span class="star" data-value="3"><i class="bi bi-star"></i></span>
+                                <span class="star" data-value="4"><i class="bi bi-star"></i></span>
+                                <span class="star" data-value="5"><i class="bi bi-star"></i></span>
                             </div>
-                            <span class="card-subtitle">€' . $product->price . '</span>
                         </div>
-                        <div>';
+                        <div class="d-flex flex-column justify-content-around align-items-end g-3 mt-1">
+                            <span class="card-subtitle h4 fw-bold text-danger">€' . $product->price . '</span>';
                         if($this->model->isUserLoggedIn()) {
                             $this->showActionForm('shoppingcart', 'addtocart', 'Add to cart', $product->id);
                         }
