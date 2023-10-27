77d2716076fd1c849aead28cfaf23fcd4948b832
Corby7 <corbijn.bulsink@hotmail.com>
2023-10-27 17:37:08 +0200

    finished 4 crud functions #12

--- cruds/ratingcrud.php
+++ cruds/ratingcrud.php
@@ -8,15 +8,32 @@ class RatingCrud {
         $this->crud = $crud;
     }
 
-    public function saveRatingProduct($productId, $UserId, $rating) {}
-
-    public function adjustRatingProduct($productId, $userId, $rating) {}
+    public function saveProductRating($productId, $userId, $rating) {
+        $sql = "INSERT INTO ratings (user_id, product_id, rating) VALUES (:user_id, :product_id, :rating)";
+        $params = ['user_id' => $userId, 'product_id' => $productId, 'rating' => $rating];
+        return $this->crud->createRow($sql, $params);
+    }
 
     public function getProductRating($productId) {
-        $sql = "SELECT rating FROM products WHERE id = :id";
-        $params = ['id' => $productId];
+        $sql = "SELECT ROUND(AVG(rating), 0) AS average FROM ratings WHERE product_id = :product_id";
+        $params = ['product_id' => $productId];
         return $this->crud->readOneRow($sql, $params);
     }
+
+    public function ratingExists($productId, $userId) {
+        $sql = "SELECT CASE WHEN EXISTS (SELECT 1 FROM ratings WHERE product_id = :product_id AND user_id = :user_id) THEN 1 ELSE 2 END AS result";
+        $params = ['product_id' => $productId, 'user_id' => $userId];
+        return $this->crud->readOneRow($sql, $params);
+    }
+
+    public function updateProductRating($productId, $userId, $rating) {
+        $sql = "UPDATE ratings SET rating = :rating WHERE product_id = :product_id AND user_id = :user_id";
+        $params = ['rating' => $rating,'product_id' => $productId, 'user_id' => $userId];
+        return $this->crud->updateRow($sql, $params);
+    }
+
+
+    //SELECT id, user_id, product_id, ROUND(AVG(rating), 0) AS average FROM `ratings` GROUP BY product_id; 
 
     public function getAllRatings($productIds) {}
 
